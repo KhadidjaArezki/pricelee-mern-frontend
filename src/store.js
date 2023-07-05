76c73292942121519ed509cb8c712d52de3c631e
Khadidja Arezki <khadidjaarezki999@gmail.com>
2023-07-05 22:49:00 +0100

    Add authReducer and apiSlice to redux store and remove usersReducer

--- src/store.js
+++ src/store.js
@@ -1,16 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './reducers/userReducer'
-import searchReducer from './reducers/searchReducer'
-import trackerReducer from './reducers/trackerReducer'
-import notificationReducer from './reducers/notificationReducer'
+import { configureStore } from "@reduxjs/toolkit"
+import searchReducer from "./reducers/searchReducer"
+import trackerReducer from "./reducers/trackerReducer"
+import notificationReducer from "./reducers/notificationReducer"
+import authReducer from "./reducers/authReducer"
+import { apiSlice } from "./reducers/apiSlice"
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
     search: searchReducer,
     tracker: trackerReducer,
-    notification: notificationReducer
-  }
+    notification: notificationReducer,
+    auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  // This required for RTK Query to cache results
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 })
 
 export default store
