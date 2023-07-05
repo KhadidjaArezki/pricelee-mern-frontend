29c731a50ecdb97ddfe77033fbe3859e8c4b85c7
Khadidja Arezki <khadidjaarezki999@gmail.com>
2023-07-05 22:52:42 +0100

    Create RequireAuth component to separate auth logic from Tracker

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Home from './containers/Home'
-import Search from './containers/Search'
-import Signup from './containers/Signup'
-import Tracker from './containers/Tracker'
-import NotFound from './components/NotFound'
-import Notification from './components/Notification'
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Home from "./containers/Home"
+import Search from "./containers/Search"
+import Signup from "./containers/Signup"
+import Tracker from "./containers/Tracker"
+import NotFound from "./components/NotFound"
+import Notification from "./components/Notification"
+import RequireAuth from "./containers/RequireAuth"
+import { Switch } from "react-router-dom/cjs/react-router-dom.min"
 
 function App() {
   return (
     <>
-      <Notification/>
+      <Notification />
       <Router>
-          <Switch>
-            <Route exact path='/'>
-              <Home/>
-            </Route>
-            <Route path='/signup'>
-              <Signup/>
-            </Route>
-            <Route path='/search'>
-              <Search/>
-            </Route>
-            <Route path='/tracker'>
-              <Tracker/>
-            </Route>
-            <Route path='*'>
-              <NotFound />
-            </Route>
-          </Switch>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          {/* Protected Routes */}
+          <Route path="/tracker">
+            <RequireAuth />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
       </Router>
     </>
   )
